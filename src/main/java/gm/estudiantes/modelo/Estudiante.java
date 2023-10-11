5d129c95a5b1034676ba4056ba590db40d9da057
jonathanrodvaz <jonathanrodvazquez@gmail.com>
2023-10-11 07:23:46 +0200

    añadida la clase estudiante con notaciones de lombok

---
+++ src/main/java/gm/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,31 @@
+package gm.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+// BoilerPlate = Codigo Repetitivo
+//Para evitar codigo repetitivo, añadimos lo siguiente
+//Con Data lo que hacemos es que se generen los metodos get y set de manera automatica, gracias a lombok
+@Data
+//Con la siguiente notacion añadimos los constructores vacios y el metodo ToString
+@NoArgsConstructor
+@ToString
+
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+}
